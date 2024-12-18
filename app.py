0c1ac989c9da2d95e36a14c4473c853049f5749e
igor ovsyannikov <iovs5915@gmail.com>
2024-12-18 03:55:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 @app.route('/') #127.1.1.0:5000/
 def index():
-    return "Hello!"
+    return render_template("index.html")
 
 if __name__ == ('__main__'):
     app.run()
